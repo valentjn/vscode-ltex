a0313de859843679b23e15218b12efe2967ef36c
Adam Voss <vossad01@gmail.com>
2017-04-24 19:19:19 -0500

    Initial proof of concept

---
+++ README.md
@@ -0,0 +1,12 @@
+# LanguageTool Extension for Visual Studio Code
+
+Provides offline grammar checking in Visual Studio Code using [LanguageTool](https://languagetool.org/) via the [LanguageTool Language Server](https://github.com/adamvoss/languagetool-languageserver).
+
+## Features
+
+## Requirements
+Java 8+ is required.
+
+## Extension Settings
+
+## Known Issues
---
+++ src/extension.ts
@@ -0,0 +1,109 @@
+'use strict';
+
+import * as fs from 'fs';
+import * as path from 'path';
+import * as net from 'net';
+import * as child_process from "child_process";
+
+import { workspace, Disposable, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, StreamInfo } from 'vscode-languageclient';
+
+export function activate(context: ExtensionContext) {
+
+	function createServer(): Promise<StreamInfo> {
+		return new Promise((resolve, reject) => {
+			var server = net.createServer((socket) => {
+				console.log("Creating server");
+
+				resolve({
+					reader: socket,
+					writer: socket
+				});
+
+				socket.on('end', () => console.log("Disconnected"));
+			}).on('error', (err) => {
+				// handle errors here
+				throw err;
+			});
+
+			let javaExecutablePath = findJavaExecutable('java');
+
+			// grab a random port.
+			server.listen(() => {
+				// Start the child java process
+				let options = { cwd: workspace.rootPath };
+
+				let args = [
+					'-jar',
+					path.resolve(context.extensionPath, '..', 'server', 'build', 'libs', 'languagetool-languageserver-0.0-SNAPSHOT-all.jar'),
+					server.address().port.toString()
+				]
+
+				let process = child_process.spawn(javaExecutablePath, args, options);
+
+				// Send raw output to a file
+				if (!fs.existsSync(context.storagePath))
+					fs.mkdirSync(context.storagePath);
+
+				let logFile = context.storagePath + '/vscode-languagetool-languageserver.log';
+				let logStream = fs.createWriteStream(logFile, { flags: 'w' });
+
+				process.stdout.pipe(logStream);
+				process.stderr.pipe(logStream);
+
+				console.log(`Storing log in '${logFile}'`);
+			});
+		});
+	};
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: ['plaintext'],
+		synchronize: {}
+	}
+
+	// Create the language client and start the client.
+	let disposable = new LanguageClient('languageTool', 'LanguageTool Client', createServer, clientOptions).start();
+
+	// Push the disposable to the context's subscriptions so that the 
+	// client can be deactivated on extension deactivation
+	context.subscriptions.push(disposable);
+}
+
+// MIT Licensed code from: https://github.com/georgewfraser/vscode-javac
+function findJavaExecutable(binname: string) {
+	binname = correctBinname(binname);
+
+	// First search each JAVA_HOME bin folder
+	if (process.env['JAVA_HOME']) {
+		let workspaces = process.env['JAVA_HOME'].split(path.delimiter);
+		for (let i = 0; i < workspaces.length; i++) {
+			let binpath = path.join(workspaces[i], 'bin', binname);
+			if (fs.existsSync(binpath)) {
+				return binpath;
+			}
+		}
+	}
+
+	// Then search PATH parts
+	if (process.env['PATH']) {
+		let pathparts = process.env['PATH'].split(path.delimiter);
+		for (let i = 0; i < pathparts.length; i++) {
+			let binpath = path.join(pathparts[i], binname);
+			if (fs.existsSync(binpath)) {
+				return binpath;
+			}
+		}
+	}
+
+	// Else return the binary name directly (this will likely always fail downstream) 
+	return null;
+}
+
+function correctBinname(binname: string) {
+	if (process.platform === 'win32')
+		return binname + '.exe';
+	else
+		return binname;
+}
