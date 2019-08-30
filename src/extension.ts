52155a27a9c5335078e8b03ccde93364faf15fd6
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2019-08-30 17:05:51 +0200

    Reformat with spaces

--- src/extension.ts
+++ src/extension.ts
@@ -10,117 +10,116 @@ import { LanguageClient, LanguageClientOptions, SettingMonitor, StreamInfo } fro
 
 export function activate(context: ExtensionContext) {
 
-	function discoverExtensionPaths() {
-		return extensions.all
-			.filter(x => x.id.startsWith("valentjn.vscode-languagetool-"))
-			.map(x => x.extensionPath)
-	}
+  function discoverExtensionPaths() {
+    return extensions.all
+      .filter(x => x.id.startsWith("valentjn.vscode-languagetool-"))
+      .map(x => x.extensionPath)
+  }
 
-	function buildDesiredClasspath() {
-		const isWindows = process.platform === 'win32';
+  function buildDesiredClasspath() {
+    const isWindows = process.platform === 'win32';
 
-		const joinCharacter = isWindows ? ';' : ':'
+    const joinCharacter = isWindows ? ';' : ':'
 
-		const additionalPaths = discoverExtensionPaths()
-			.map(p => path.resolve(context.extensionPath, '..', p, 'lib', '*'))
-			.join(joinCharacter);
+    const additionalPaths = discoverExtensionPaths()
+      .map(p => path.resolve(context.extensionPath, '..', p, 'lib', '*'))
+      .join(joinCharacter);
 
-		let desiredClasspath = path.join('lib', '*');
-		if (additionalPaths) {
-			desiredClasspath += joinCharacter + additionalPaths
-		}
-		return desiredClasspath
-	}
+    let desiredClasspath = path.join('lib', '*');
+    if (additionalPaths) {
+      desiredClasspath += joinCharacter + additionalPaths
+    }
+    return desiredClasspath
+  }
 
-	function setClasspath(text: String, desiredClasspath: String): String {
-		const classpathRegexp = /^((?:set )?CLASSPATH=[%$]APP_HOME%?[\\\/])(.*)$/m
+  function setClasspath(text: String, desiredClasspath: String): String {
+    const classpathRegexp = /^((?:set )?CLASSPATH=[%$]APP_HOME%?[\\\/])(.*)$/m
 
-		return text.replace(classpathRegexp, `$1${desiredClasspath}`)
-	}
+    return text.replace(classpathRegexp, `$1${desiredClasspath}`)
+  }
 
-	function createServer(): Promise<StreamInfo> {
-		return new Promise((resolve, reject) => {
-			var server = net.createServer((socket) => {
-				console.log("Creating server");
+  function createServer(): Promise<StreamInfo> {
+    return new Promise((resolve, reject) => {
+      var server = net.createServer((socket) => {
+        console.log("Creating server");
 
-				resolve({
-					reader: socket,
-					writer: socket
-				});
+        resolve({
+          reader: socket,
+          writer: socket
+        });
 
-				socket.on('end', () => console.log("Disconnected"));
-			}).on('error', (err) => {
-				// handle errors here
-				throw err;
-			});
+        socket.on('end', () => console.log("Disconnected"));
+      }).on('error', (err) => {
+        // handle errors here
+        throw err;
+      });
 
-			let isWindows = process.platform === 'win32';
+      let isWindows = process.platform === 'win32';
 
-			// grab a random port.
-			server.listen(() => {
-				// Start the child java process
-				let options = { cwd: workspace.rootPath };
+      // grab a random port.
+      server.listen(() => {
+        // Start the child java process
+        let options = { cwd: workspace.rootPath };
 
-				const scriptDir = path.resolve(context.extensionPath, 'lib', 'languagetool-languageserver', 'build', 'install', 'languagetool-languageserver', 'bin')
-				let originalScript = path.resolve(scriptDir, isWindows ? 'languagetool-languageserver.bat' : 'languagetool-languageserver')
-				const newScript = path.resolve(scriptDir, isWindows ? 'languagetool-languageserver-live.bat' : 'languagetool-languageserver-live')
+        const scriptDir = path.resolve(context.extensionPath, 'lib', 'languagetool-languageserver', 'build', 'install', 'languagetool-languageserver', 'bin')
+        let originalScript = path.resolve(scriptDir, isWindows ? 'languagetool-languageserver.bat' : 'languagetool-languageserver')
+        const newScript = path.resolve(scriptDir, isWindows ? 'languagetool-languageserver-live.bat' : 'languagetool-languageserver-live')
 
-				const scriptText = fs.readFileSync(originalScript, "utf8")
-				const newText = setClasspath(scriptText, buildDesiredClasspath())
-				fs.writeFileSync(newScript, newText, { mode: 0o777 })
+        const scriptText = fs.readFileSync(originalScript, "utf8")
+        const newText = setClasspath(scriptText, buildDesiredClasspath())
+        fs.writeFileSync(newScript, newText, { mode: 0o777 })
 
-				let process = child_process.spawn(newScript, [server.address().port.toString()], options);
+        let process = child_process.spawn(newScript, [server.address().port.toString()], options);
 
-				// Send raw output to a file
-				let logFile = '/tmp/vscode-languagetool-languageserver.log';
-				let logStream = fs.createWriteStream(logFile, { flags: 'w' });
+        // Send raw output to a file
+        let logFile = '/tmp/vscode-languagetool-languageserver.log';
+        let logStream = fs.createWriteStream(logFile, { flags: 'w' });
 
-				process.stdout.pipe(logStream);
-				process.stderr.pipe(logStream);
+        process.stdout.pipe(logStream);
+        process.stderr.pipe(logStream);
 
-				console.log(`Storing log in '${logFile}'`);
-			});
-		});
-	};
+        console.log(`Storing log in '${logFile}'`);
+      });
+    });
+  };
 
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		documentSelector: ['plaintext', 'markdown', 'latex'],
-		synchronize: {
-			configurationSection: 'languageTool'
-		}
-	}
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: ['plaintext', 'markdown', 'latex'],
+    synchronize: {
+      configurationSection: 'languageTool'
+    }
+  }
 
-	// Allow to enable languageTool in specific workspaces
-	let config = workspace.getConfiguration('languageTool');
+  // Allow to enable languageTool in specific workspaces
+  let config = workspace.getConfiguration('languageTool');
 
-	if (config['enabled']) {
-		// create the language client
-		let languageClient = new LanguageClient(
-				'languageTool', 'LanguageTool Client', createServer, clientOptions);
+  if (config['enabled']) {
+    // create the language client
+    let languageClient = new LanguageClient(
+        'languageTool', 'LanguageTool Client', createServer, clientOptions);
 
-		// Hack to enable the server to add words to the dictionary.
-		// The client configuration cannot be changed directly by the server, so we send a
-		// telemetry notification to the client, which then adds the word to the dictionary.
-		languageClient.onTelemetry((param) =>
-				{
-					const prefix = "languageTool.addToDictionary ";
-					if (param.startsWith(prefix)) {
-						const word: string = param.substring(prefix.length);
-						let dictionary: string[] = config['dictionary'];
-						dictionary.push(word);
-						dictionary.sort((a: string, b: string) =>
-								a.localeCompare(b, undefined, {sensitivity: 'base'}));
-						config.update('dictionary', dictionary, ((workspace.rootPath) ? true : undefined));
-					} else {
-						console.warn(`vscode-languagetool: Unknown telemetry event "${param}"`);
-					}
-				});
+    // Hack to enable the server to add words to the dictionary.
+    // The client configuration cannot be changed directly by the server, so we send a
+    // telemetry notification to the client, which then adds the word to the dictionary.
+    languageClient.onTelemetry((param) => {
+      const prefix = "languageTool.addToDictionary ";
+      if (param.startsWith(prefix)) {
+        const word: string = param.substring(prefix.length);
+        let dictionary: string[] = config['dictionary'];
+        dictionary.push(word);
+        dictionary.sort((a: string, b: string) =>
+            a.localeCompare(b, undefined, { sensitivity: 'base' }));
+        config.update('dictionary', dictionary, ((workspace.rootPath) ? true : undefined));
+      } else {
+        console.warn(`vscode-languagetool: Unknown telemetry event "${param}"`);
+      }
+    });
 
-		let disposable = languageClient.start();
+    let disposable = languageClient.start();
 
-		// Push the disposable to the context's subscriptions so that the
-		// client can be deactivated on extension deactivation
-		context.subscriptions.push(disposable);
-	}
+    // Push the disposable to the context's subscriptions so that the
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
+  }
 }
