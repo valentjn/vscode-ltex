bd285b88a48dcfb857a1c531899cb755d71df898
Julian Valentin <julian.valentin@bsplines.org>
2020-05-17 15:11:41 +0200

    Improve logging in runTests

--- test/runTests.ts
+++ test/runTests.ts
@@ -11,35 +11,49 @@ async function main(): Promise<void> {
   let tmpDirPath: string | undefined;
 
   try {
+    console.log('Downloading and installing VS Code...');
     const vscodeExecutablePath: string = await CodeTest.downloadAndUnzipVSCode('stable');
+
+    console.log('Downloading and installing VS Code...');
     const cliPath: string = CodeTest.resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
-    tmpDirPath = Fs.mkdtempSync(Path.join(Path.dirname(vscodeExecutablePath), 'tmp-'));
+
+    const tmpDirPrefix: string = Path.join(Path.dirname(vscodeExecutablePath), 'tmp-');
+    console.log(`Creating temporary directory with prefix '${tmpDirPrefix}'...`);
+    tmpDirPath = Fs.mkdtempSync(tmpDirPrefix);
+    console.log(`Created temporary directory '${tmpDirPath}'.`);
+
     const userDataDirPath: string = Path.join(tmpDirPath, 'user');
     const extensionsDirPath: string = Path.join(tmpDirPath, 'extensions');
 
+    console.log('Installing extensions...');
     const process: ChildProcess.SpawnSyncReturns<string> = ChildProcess.spawnSync(cliPath, [
           '--user-data-dir', userDataDirPath,
           '--extensions-dir', extensionsDirPath,
           '--install-extension', 'james-yu.latex-workshop',
-          '--install-extension', Path.join(__dirname, `../../vscode-ltex-${ltexVersion}.vsix`),
+          '--install-extension', Path.join(__dirname, '..', '..',
+            `vscode-ltex-${ltexVersion}.vsix`),
         ], {
           encoding: 'utf-8',
           stdio: 'inherit',
         });
+
     if (process.status != 0) throw new Error('Could not install extensions.');
 
+    console.log('Running tests...');
     exitCode = await CodeTest.runTests({
           vscodeExecutablePath: vscodeExecutablePath,
           launchArgs: [
             '--user-data-dir', userDataDirPath,
             '--extensions-dir', extensionsDirPath,
           ],
-          extensionDevelopmentPath: Path.join(__dirname, '../../'),
+          extensionDevelopmentPath: Path.join(__dirname, '..', '..'),
           extensionTestsPath: Path.join(__dirname, './index'),
         });
   } catch (e) {
     console.error('Failed to run tests');
-    console.error(e.stack);
+    console.error(`Error name: ${e.name}`);
+    console.error(`Error message: ${e.message}`);
+    console.error(`Error stack: ${e.stack}`);
   } finally {
     if (tmpDirPath != null) Rimraf.sync(tmpDirPath);
   }
